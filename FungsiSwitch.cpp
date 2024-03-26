451fe012e4a1fc9b6e4773601a69b12a28621d12
naufaalkh <naufaalkh@gmail.com>
2024-03-26 14:56:32 +0700

    membuat menu pilihan

--- FungsiSwitch.cpp
+++ FungsiSwitch.cpp
@@ -27,7 +27,30 @@ int perkalian(int a, int b)
     return a * b;
 }
 
-float pembagian(int a, int b)
+float pembagian(float a, float b)
 {
     return a / b;
+}
+
+int main()
+{
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukkan menu pilihan : " << endl;
+        cin >> pilihan;
+
+        switch (pilihan)
+
+      
+    }
+
 }
