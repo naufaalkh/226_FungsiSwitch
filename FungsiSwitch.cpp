2b227c5e3199d0ada011010a48a4c866f32957c0
naufaalkh <naufaalkh@gmail.com>
2024-03-26 14:41:22 +0700

    Membuat prosedur

--- FungsiSwitch.cpp
+++ FungsiSwitch.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int bilangan1, bilangan2; // input variabel global
+
+void inputData()
+{
+    cout << "Masukkan bilangan pertama :";
+    cin >> bilangan1;
+    cout << "Masukkan bilangan kedua :";
+    cin >> bilangan2;
+}
