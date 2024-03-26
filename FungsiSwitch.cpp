f3237594fa51adb55b6e8effe3d5c0041f681cb0
naufaalkh <naufaalkh@gmail.com>
2024-03-26 15:25:51 +0700

    menambah line break

--- FungsiSwitch.cpp
+++ FungsiSwitch.cpp
@@ -53,18 +53,22 @@ int main()
             case 1:
             inputData();
             cout << "Hasil penjumlahan =" << penjumlahan(bilangan1, bilangan2) << endl;
+            break;
 
             case 2:
             inputData();
             cout << "Hasil pengurangan =" << pengurangan(bilangan1, bilangan2) << endl;
+            break;
 
             case 3:
             inputData();
             cout << "Hasil perkalian =" << perkalian(bilangan1, bilangan2) << endl;
+            break;
 
             case 4:
             inputData();
             cout << "Hasil pembagian =" << pembagian(bilangan1, bilangan2) << endl;
+            break;
 
             case 5:
                 break;
